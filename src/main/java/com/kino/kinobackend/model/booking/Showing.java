adac3422fcf6ab7a2d3daea1b319ed34edde2818
MaVirgil <magnusvirgil@hotmail.com>
2026-03-05 09:43:13 +0100

    set fields and JPA annotations

--- src/main/java/com/kino/kinobackend/model/booking/Showing.java
+++ src/main/java/com/kino/kinobackend/model/booking/Showing.java
@@ -1,4 +1,28 @@
 package com.kino.kinobackend.model.booking;
 
+import com.kino.kinobackend.model.movie.Movie;
+import com.kino.kinobackend.model.theater.Theater;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
 public class Showing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private double price;
+    private LocalDateTime time;
+
+    @ManyToOne
+    private Movie movie;
+
+    @ManyToOne
+    private Theater theater;
 }
