6a1d280f7316d038fb0eea6191b11264fe886dcb
MaVirgil <magnusvirgil@hotmail.com>
2026-03-06 11:21:43 +0100

    add wildcard CORS annotation

--- src/main/java/com/kino/kinobackend/controller/TheaterController.java
+++ src/main/java/com/kino/kinobackend/controller/TheaterController.java
@@ -7,10 +7,7 @@ import com.kino.kinobackend.service.theater.TheaterService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +15,7 @@ import java.util.Optional;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/theater")
+@CrossOrigin("*")
 public class TheaterController {
     private final RowService rowService;
     private final SeatService seatService;
