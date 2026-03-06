46f2f96558b4f126d904760a62865319dec750f9
MaVirgil <magnusvirgil@hotmail.com>
2026-03-06 11:21:16 +0100

    use lombok for constructors

--- src/main/java/com/kino/kinobackend/service/theater/TheaterService.java
+++ src/main/java/com/kino/kinobackend/service/theater/TheaterService.java
@@ -2,19 +2,17 @@ package com.kino.kinobackend.service.theater;
 
 import com.kino.kinobackend.model.theater.Theater;
 import com.kino.kinobackend.repository.theater.TheaterRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class TheaterService {
 
     private final TheaterRepository repository;
-
-    public TheaterService(TheaterRepository repository){
-        this.repository = repository;
-    }
 
     public List<Theater> findAll(){
         return this.repository.findAll();
