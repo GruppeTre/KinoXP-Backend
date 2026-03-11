f294c816f057ca351b0fab8a940690e7472e4b3d
Isabellabmj <Isabellajoensen@gmail.com>
2026-03-11 13:48:46 +0100

    .

--- src/main/java/com/kino/kinobackend/controller/MovieController.java
+++ src/main/java/com/kino/kinobackend/controller/MovieController.java
@@ -6,15 +6,13 @@ import com.kino.kinobackend.repository.movie.MovieRepository;
 import com.kino.kinobackend.service.movie.MovieService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/movie")
 public class MovieController {
